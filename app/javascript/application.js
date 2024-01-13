e6b993224ca8179c6f8daed31e4c359fcf8050c1
aron-net <aronmilahela@gmail.com>
2024-01-13 23:49:30 +0300

    feat(dev)- create the home page view, add the search box to the home page view and update the Rroute

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.addEventListener('DOMContentLoaded', () => {
+    const searchBox = document.getElementById('search-box');
+    searchBox.addEventListener('input', (event) => {
+      const searchTerm = event.target.value;
+      console.log(searchTerm); 
+    });
+  });
